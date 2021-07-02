59a0d653263641a3e2c4877ac16924a6d5b29499
Robert Ottesen <robert.ottesen@solace.com>
2021-07-02 10:30:22 -0400

    Release 1.1.0

---
+++ src/main/java/com/solace/swim/util/DestinationResolver.java
@@ -0,0 +1,66 @@
+package com.solace.swim.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+import javax.annotation.PostConstruct;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
+
+public class DestinationResolver implements Serializable {
+    private static final Logger logger = LoggerFactory.getLogger(DestinationResolver.class);
+    private static final long serialVersionUID = 1L;
+
+    @Autowired
+    ResourceLoader resourceLoader;
+
+    private String xslFileName;
+    private StreamSource styleSource;
+    private Transformer transformer;
+
+    public DestinationResolver(String stylesheetTransformationFileName) throws TransformerConfigurationException, IOException {
+        xslFileName = "classpath:" + stylesheetTransformationFileName;
+    }
+
+    @PostConstruct
+    public void init() throws IOException, TransformerConfigurationException {
+        Resource xslResource = resourceLoader.getResource(xslFileName);
+        //styleSource = new StreamSource(new File((getClass().getClassLoader().getResource(stylesheetTransformationFileName)).getFile()));
+        styleSource = new StreamSource(xslResource.getInputStream());
+        TransformerFactory factory = TransformerFactory.newInstance();
+        transformer = factory.newTransformer(styleSource);
+    }
+
+    public String computeDestination(String payload) {
+        if (logger.isDebugEnabled()) {
+            logger.info("Entering computeDestination...");
+        }
+
+        StringBuffer topic = new StringBuffer();
+        StreamSource source = new StreamSource(new ByteArrayInputStream(payload.getBytes()));
+
+        StreamResult result = null;
+        try {
+            result = new StreamResult(new ByteArrayOutputStream());
+            transformer.transform(source, result);
+            topic.append(result.getOutputStream().toString().replaceAll("\\s+", ""));
+            if (logger.isDebugEnabled()) {
+                logger.debug("Destination: " + topic.toString());
+            }
+        } catch (TransformerException e) {
+            logger.error(e.getMessage());
+        }
+        return topic.toString();
+    }
+}
---
+++ src/main/resources/fdps-json-topic.xsl
@@ -0,0 +1,18 @@
+<?xml version="1.0"?>
+<xsl:stylesheet version="2.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:ns1="http://www.opengis.net/gml/3.2" xmlns:ns2="http://www.fixm.aero/base/3.0" xmlns:ns3="http://www.fixm.aero/flight/3.0" xmlns:ns4="http://www.fixm.aero/foundation/3.0" xmlns:ns5="http://www.faa.aero/nas/3.0">
+    <xsl:output method="text" encoding="UTF-8"/>
+    <xsl:template match="/message">
+        <xsl:text>FDPS/position/</xsl:text>
+        <xsl:value-of select="flight/flightPlan/@identifier"/><xsl:text>/</xsl:text>
+        <xsl:value-of select="flight/flightStatus/@fdpsFlightStatus"/><xsl:text>/</xsl:text>
+        <xsl:value-of select="flight/flightIdentification/@aircraftIdentification"/><xsl:text>/</xsl:text>
+        <xsl:value-of select="flight/departure/@departurePoint"/><xsl:text>/</xsl:text>
+        <xsl:value-of select="flight/arrival/@arrivalPoint"/><xsl:text>/</xsl:text>
+        <xsl:value-of select="format-number(number(substring-before(flight/enRoute/position/position/location/pos,' ')),'000.0000;-000.0000')"/><xsl:text>/</xsl:text>
+        <xsl:value-of select="format-number(number(substring-after(flight/enRoute/position/position/location/pos,' ')),'0000.0000;-000.0000')"/><xsl:text>/</xsl:text>
+        <xsl:value-of select="format-number(number(flight/enRoute/position/actualSpeed/surveillance),'000')"/><xsl:text>/</xsl:text>
+        <xsl:value-of select="format-number(number(flight/enRoute/position/altitude),'00000')"/><xsl:text>/</xsl:text>
+        <xsl:value-of select="format-number(number(flight/enRoute/position/trackVelocity/x),'0000;-000')"/><xsl:text>/</xsl:text>
+        <xsl:value-of select="format-number(number(flight/enRoute/position/trackVelocity/y),'0000;-000')"/>
+    </xsl:template>
+</xsl:stylesheet>
